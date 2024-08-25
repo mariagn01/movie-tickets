1a32ba486aaa47047cfba4f48461da5cbe351b69
Maria Granli Nielsen <143001624+mariagn01@users.noreply.github.com>
2024-08-25 16:23:14 +0200

    Added the dropdown menu for choosing a movie

--- src/main/java/webprog/movietickets/TicketController.java
+++ src/main/java/webprog/movietickets/TicketController.java
@@ -1,5 +1,6 @@
 package webprog.movietickets;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -7,5 +8,16 @@ import java.util.ArrayList;
 @RestController
 public class TicketController {
 
-    private final ArrayList<Ticket> tickets = new ArrayList<Ticket>();
+    private final ArrayList<Ticket> tickets = new ArrayList<>();
+
+    @GetMapping("/getMovies")
+    public ArrayList<String> getMovies () {
+        ArrayList<String> movielist = new ArrayList<>();
+        movielist.add("Oppenheimer");
+        movielist.add("Barbie");
+        movielist.add("Dune");
+        movielist.add("The Crow");
+        movielist.add("Deadpool & Wolverine");
+        return movielist;
+    }
 }
--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,12 +1,21 @@
 $(function (){
     getMovies();
 })
+
+//Functions for retrieving available movies and displaying them in a dropdown menu
 function getMovies () {
-
+    $.get("/getMovies", function (movielist) {
+        displayMovieList(movielist)
+    })
 }
-
-function displayMovieList() {
-
+function displayMovieList(movielist) {
+    let movies = "<select class='form-select' name='movie' id='movie'>" +
+        "<option selected disabled placeholder='Choose movie'>Choose movie</option>";
+    for (const movie of movielist) {
+        movies += "<option value='" + movie + "'>" + movie + "</option>";
+    }
+    movies += "</select>"
+    $("#movie-list").html(movies);
 }
 
 function purchase() {
