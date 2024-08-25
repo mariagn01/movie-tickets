bfd3a3ea20a7cb4670b1210772c6b596d1e0cc32
Maria Granli Nielsen <143001624+mariagn01@users.noreply.github.com>
2024-08-25 17:41:23 +0200

    Finishing server code for saving and retrieving tickets

--- src/main/java/webprog/movietickets/TicketController.java
+++ src/main/java/webprog/movietickets/TicketController.java
@@ -1,15 +1,21 @@
 package webprog.movietickets;
 
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class TicketController {
+
+    @Autowired
+    public TicketRepository rep;
 
     @GetMapping("/getMovies")
     public ArrayList<String> getMovies () {
@@ -24,6 +30,25 @@ public class TicketController {
 
     @PostMapping("/saveTicket")
     public void saveTicket(Ticket ticket, HttpServletResponse response) throws IOException {
+        if(!rep.saveTicket(ticket)) {
+            response.sendError(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Error in database, could not save ticket");
+        }
+    }
 
+    @GetMapping("/getTickets")
+    public List<Ticket> getTickets (HttpServletResponse response) throws IOException {
+        //Using try/catch for errors that occur during the retrieval of tickets
+        try {
+            List<Ticket> tickets = rep.getTickets();
+            //Using if-statement to handle cases where the tickets are null or empty
+            if (tickets == null) {
+                response.sendError(HttpStatus.NO_CONTENT.value(), "No tickets found");
+                return null;
+            }
+            return tickets;
+        } catch (Exception e) {
+            response.sendError(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Error in database: "+e);
+            return null;
+        }
     }
 }
--- src/main/java/webprog/movietickets/TicketRepository.java
+++ src/main/java/webprog/movietickets/TicketRepository.java
@@ -3,8 +3,11 @@ package webprog.movietickets;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class TicketRepository {
@@ -14,7 +17,25 @@ public class TicketRepository {
 
     Logger logger = LoggerFactory.getLogger(TicketRepository.class);
 
-    public boolean saveTicket (Ticket ticket) {
-        String sql = "INSERT INTO "
+    public boolean saveTicket(Ticket ticket) {
+        String sql = "INSERT INTO Ticket (movie, amount, firstname, lastname, phone, email) VALUES(?,?,?,?,?,?)";
+        try {
+            db.update(sql, ticket.getMovie(), ticket.getAmount(), ticket.getFirstname(), ticket.getLastname(), ticket.getPhone(), ticket.getEmail());
+            return true;
+        } catch (Exception e) {
+            logger.error("Error in saveTicket: " + e);
+            return false;
+        }
+    }
+
+    public List<Ticket> getTickets() {
+        String sql = "SELECT * FROM Ticket";
+        try {
+            List<Ticket> allTickets = db.query(sql, new BeanPropertyRowMapper<>(Ticket.class));
+            return allTickets;
+        } catch (Exception e) {
+            logger.error("Error in getTickets: "+e);
+            return null;
+        }
     }
 }
