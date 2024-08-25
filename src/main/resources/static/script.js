a501d75ad1ab81588cfddbaf28c55a84a51c91d9
Maria Granli Nielsen <143001624+mariagn01@users.noreply.github.com>
2024-08-25 16:49:24 +0200

    Changed error handling back to inline error messages, as this is better and more clear for the user.

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -21,3 +21,45 @@ function displayMovieList(movielist) {
 function purchase() {
 
 }
+
+function validateInputs() {
+    let movie = $("#movie").val()
+    let amount = $("#amount").val()
+    let firstname = $("#firstname").val()
+    let lastname = $("#lastname").val()
+    let phone = $("#phone").val()
+    let email = $("#email").val()
+
+    //Removing previous error messages, if any
+    $("#error").text("");
+
+    //Validation for each input field, displaying inline errors to user
+    if (!movie) {
+        $("#movie-error").text("Please select a movie");
+        return false;
+    }
+    if ((amount <= 0) || (amount === "")) {
+        $("#amount-error").text("Enter a valid amount");
+        return false;
+    }
+    if (firstname === "") {
+        $("#firstname-error").text("Please enter a first name")
+        return false;
+    }
+    if (lastname === "") {
+        $("#lastname-error").text("Please enter a last name")
+        return false;
+    }
+    //Using regex to validate phone and email format
+    let phoneRegex = /^\+?(\d[\d\s-]{7,14}\d)$/
+    if (phone === "" || phoneRegex.test(phone)) {
+        $("#phone-error").text("Please enter a valid phone number")
+        return false;
+    }
+    let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (email === "" || !emailRegex.test(email)) {
+        $("#email-error").text("Please enter a valid e-mail")
+        return false;
+    }
+    return true; //returns true if every validation is ok
+}
